186e669159313a70597a9dc9a138b636f111610b
Natasha Camille <123318520+NatashaCamille@users.noreply.github.com>
2024-07-24 23:18:04 +0000

    Assignment _1 complete

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
+use std::io;
+
 fn fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
-    (fahrenheit - 32.0) * 5.0 / 9.0
+  (fahrenheit - 32.0) * 5.0 / 9.0
 }
 
 fn main() {
-    let fahrenheit = 98.6;
-    let celsius = fahrenheit_to_celsius(fahrenheit);
-    println!("{:.2} Fahrenheit is {:.2} Celsius", fahrenheit, celsius);
+  let mut fahrenheit_input = String::new();
+
+  println!("Enter a temperature in Fahrenheit:");
+  io::stdin().read_line(&mut fahrenheit_input).expect("Failed to read input");
+
+  let fahrenheit = fahrenheit_input.trim().parse::<f64>().expect("Invalid input");
+
+  let celsius = fahrenheit_to_celsius(fahrenheit);
+
+  println!("Temperature in Fahrenheit: {:.2}°F", fahrenheit);
+  println!("Temperature in Celsius: {:.2}°C", celsius);
 }
